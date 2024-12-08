a04f7b6e1affb15f1a27036cf484bb018394e6de
haikunn11 <phamhaicute9@gmail.com>
2024-12-09 00:39:10 +0700

    post-project

---
+++ BTVN/post-project/routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\HomeController;
+
+Route::get('/', [PostController::class, "index"]);
