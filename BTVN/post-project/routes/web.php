a62c03af0bb42f5f11e5a45229951378f824097c
haikunn11 <phamhaicute9@gmail.com>
2024-12-08 23:56:07 +0700

    Delete BTVN/post-project directory

--- BTVN/post-project/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\HomeController;
-
-Route::get('/', [PostController::class, "index"]);
